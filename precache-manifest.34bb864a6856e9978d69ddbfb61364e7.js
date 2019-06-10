cdf4ad48182881600ac51aeeb48cfd9946fc70f3
norhangit <norhan@yahoo.com>
2019-06-10 06:27:50 +0200

    Updates

---
+++ precache-manifest.34bb864a6856e9978d69ddbfb61364e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba7f33d656eccf3a4108c16c348d844",
+    "url": "/Redux_IMDB_Cinema/index.html"
+  },
+  {
+    "revision": "4d69e07df8f85d38c72f",
+    "url": "/Redux_IMDB_Cinema/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "683bf14584e37c0157dc",
+    "url": "/Redux_IMDB_Cinema/static/css/main.050a7485.chunk.css"
+  },
+  {
+    "revision": "4d69e07df8f85d38c72f",
+    "url": "/Redux_IMDB_Cinema/static/js/2.d20df482.chunk.js"
+  },
+  {
+    "revision": "683bf14584e37c0157dc",
+    "url": "/Redux_IMDB_Cinema/static/js/main.7601bba3.chunk.js"
+  },
+  {
+    "revision": "5cb474047d3110535a07",
+    "url": "/Redux_IMDB_Cinema/static/js/runtime~main.b8ad28a4.js"
+  }
+]);
