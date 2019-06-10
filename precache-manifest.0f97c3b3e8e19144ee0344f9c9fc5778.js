d2c1597b5bde8fd971fb43dca4f1bfce0c646107
norhangit <norhan@yahoo.com>
2019-06-10 06:37:26 +0200

    Updates

---
+++ precache-manifest.0f97c3b3e8e19144ee0344f9c9fc5778.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c14d6d05802f7d6e9462cb9ca42a12",
+    "url": "/Redux_imdb_cinema/index.html"
+  },
+  {
+    "revision": "4d69e07df8f85d38c72f",
+    "url": "/Redux_imdb_cinema/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a8a88015a8a37eb9a900",
+    "url": "/Redux_imdb_cinema/static/css/main.050a7485.chunk.css"
+  },
+  {
+    "revision": "4d69e07df8f85d38c72f",
+    "url": "/Redux_imdb_cinema/static/js/2.d20df482.chunk.js"
+  },
+  {
+    "revision": "a8a88015a8a37eb9a900",
+    "url": "/Redux_imdb_cinema/static/js/main.4dc0acfe.chunk.js"
+  },
+  {
+    "revision": "d1aba6a47c464ba3c46b",
+    "url": "/Redux_imdb_cinema/static/js/runtime~main.40cf3688.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Redux_IMDB_Cinema/precache-manifest.34bb864a6856e9978d69ddbfb61364e7.js"
+  "/Redux_imdb_cinema/precache-manifest.0f97c3b3e8e19144ee0344f9c9fc5778.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Redux_IMDB_Cinema/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Redux_imdb_cinema/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
